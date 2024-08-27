154618aff2904db8a25e39130a8870fb720d26ff
somulo <mcomulosammy37@gmail.com>
2024-08-27 12:47:55 +0300

    chore: add find position variable

--- Day_1/position.go
+++ Day_1/position.go
@@ -5,36 +5,43 @@ import (
 	"os"
 )
 
-func Find_Position(str string)int {
+// Find_Position calculates the position where Santa first reaches the basement (floor -1)
+func Find_Position(str string) int {
 	countfloor := 0
-	position := 0
-	// fmt.Println(puzzles)
-	for i, puzzle := range str{
+	// Iterate over each character in the puzzles
+	for i, puzzle := range str {
 		if puzzle == '(' {
 			countfloor++
-			i++
 		} else if puzzle == ')' {
 			countfloor--
-			i++
 		}
-		position += i
-		fmt.Println(i)
+		
+		// Check if Santa has entered the basement
+		if countfloor == -1 {
+			return i + 1 // Return position
+		}
 	}
-	return countfloor 
+	
+	//  if the basement is never reached
+	return -1
 }
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("USAGE: go run . something.txt")
+		fmt.Println("USAGE: go run . filename")
 		os.Exit(1)
 	}
 	str := os.Args[1]
 	puzzlebit, err := os.ReadFile(str)
 	if err != nil {
-		fmt.Printf("error reading the file")
+		fmt.Printf("error reading the file: %v\n", err)
 		os.Exit(1)
 	}
 	puzzles := string(puzzlebit)
-	Destination := Find_Position(puzzles)
-	fmt.Println("Final Destination:", Destination, "floor")
+	position := Find_Position(puzzles)
+	if position != -1 {
+		fmt.Printf("The first time santa gets basement is at position %d\n", position)
+	} else {
+		fmt.Println("Santa never enters the basement.")
+	}
 }
