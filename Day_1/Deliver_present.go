a3df836c09eb1b3bfb80b02a0c74df4ac5b57df8
somulo <mcomulosammy37@gmail.com>
2024-08-27 12:46:59 +0300

    chore: comment main function

--- Day_1/Deliver_present.go
+++ Day_1/Deliver_present.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 // count the number of "(" and the number of ")"
 func Locate_Floor(puzzles string) int {
 	countfloor := 0
@@ -20,18 +15,18 @@ func Locate_Floor(puzzles string) int {
 	return countfloor
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("USAGE: go run . something.txt")
-		os.Exit(1)
-	}
-	str := os.Args[1]
-	puzzlebit, err := os.ReadFile(str)
-	if err != nil {
-		fmt.Printf("error reading the file")
-		os.Exit(1)
-	}
-	puzzles := string(puzzlebit)
-	Destination := Locate_Floor(puzzles)
-	fmt.Println("Final Destination:", Destination,"floor")
-}
+// func main() {
+// 	if len(os.Args) != 2 {
+// 		fmt.Println("USAGE: go run . something.txt")
+// 		os.Exit(1)
+// 	}
+// 	str := os.Args[1]
+// 	puzzlebit, err := os.ReadFile(str)
+// 	if err != nil {
+// 		fmt.Printf("error reading the file")
+// 		os.Exit(1)
+// 	}
+// 	puzzles := string(puzzlebit)
+// 	Destination := Locate_Floor(puzzles)
+// 	fmt.Println("Final Destination:", Destination, "floor")
+// }
